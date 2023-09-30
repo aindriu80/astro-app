ef8b74e45e1205e0f6bb1a1971b4842c4840c629
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-09-30 16:52:00 +0100

    Adding some posts, starting post-list

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, z } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+
+// Defining some collections
+const postsCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    date: z.string(),
+    image: z.string(),
+    title: z.string(),
+  }),
+})
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  posts: postsCollection,
+})
