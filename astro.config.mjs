d5913e826a61fc343f609a752241744f2de659b4
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-10-01 20:36:11 +0100

    Rhythm Blog articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
-import { defineConfig, z } from 'astro/config'
+import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-
-// Defining some collections
-const postsCollection = defineCollection({
-  schema: z.object({
-    author: z.string(),
-    date: z.string(),
-    image: z.string(),
-    title: z.string(),
-  }),
-})
-
 // https://astro.build/config
+
 export default defineConfig({
   integrations: [tailwind()],
-  posts: postsCollection,
 })
